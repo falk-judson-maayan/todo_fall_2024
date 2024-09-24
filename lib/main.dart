ca04fd4425ba57c539ba6c0fa182bba6e506256d
falk-judson-maayan <maayan.falkjudson@gmail.com>
2024-09-24 19:21:04 +0200

    Add home and login screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -16,8 +19,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const Material(
-        child: Center(
-          child: Text('Hello World'),
+        body: LoginScreen(),
         ),
       ),
     );
